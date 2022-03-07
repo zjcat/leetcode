408becb325dd76cc511eae0f8649c8821416b0a3
zjcat <185990137@qq.com>
2022-03-07 17:22:03 +0800

    链表添加

--- src/array/lesson5.ts
+++ src/array/lesson5.ts
@@ -1,7 +1,10 @@
 /**
- * @description 用两个栈实现一个队列
+ * @description 用两个栈实现一个队列 用链表是先队列
  */
 
+ /**
+  * 两个栈实现队列
+  */
 export default class Myqueue {
   private stack1: number[] = [];
   private stack2: number[] = [];
@@ -30,5 +33,35 @@ export default class Myqueue {
   }
   length () {
     return this.stack1.length
+  }
+}
+interface IlinkNode {
+  vale:number;
+  next?:IlinkNode
+}
+//使用链表实现队列
+export class ListQueue{
+  private len:number = 0;
+  private head:IlinkNode|undefined
+  private totle:IlinkNode|undefined
+  add(n:number){
+    if(!this.totle){
+      this.totle = {
+        vale:n
+      }
+      //{value:1}
+    }else{
+      this.totle = this.totle
+     // {value:1,next:{value:2,next:{value:3}}}
+    }
+    
+    
+    this.len++
+  }
+  deleted(){
+    this.len--
+  }
+  lingth(){
+    return this.len
   }
 }
--- src/array/lesson6.ts
+++ src/array/lesson6.ts
@@ -26,6 +26,24 @@ export function reverseLinkList (link: TlinkListNode, prevNode?: TlinkListNode):
   }
   return sendReverseLink
 }
+export function reverseLinkListWhile (link: TlinkListNode): TlinkListNode {
+  let reserseLink: TlinkListNode | undefined
+  let prevNode: TlinkListNode | undefined
+  let nextNode: TlinkListNode | undefined = link
+  while (nextNode) {
+    if (reserseLink && !prevNode) {
+      delete reserseLink.next
+    }
+    if (reserseLink && prevNode) {
+      reserseLink.next = prevNode
+    }
+    reserseLink = prevNode
+    prevNode = nextNode
+    nextNode = nextNode?.next
+  }
+  reserseLink!.next = prevNode
+  return reserseLink!
+}
 
 /**
  * 数组转单向链表
@@ -48,3 +66,7 @@ export function createLinkList (arr: number[]): TlinkListNode {
   }
   return curNode
 }
+
+//链表和数组，那个实现队列更快 链表实现队列
+//数组：是连续存储，push很快，shift很慢
+//链表：非连续存储，add和delete都很快（但查找很慢）
